aa1495437e795090c04dd681552961b15b881049
Shinsei <murakami.shinsei.zn@gmail.com>
2024-09-28 13:16:13 +0900

    モーダル見た目調整

--- frontend/components/WordFormDialog.tsx
+++ frontend/components/WordFormDialog.tsx
@@ -8,192 +8,192 @@ import Button from '@mui/material/Button';
 import { PartOfSpeech, WordType } from '@/types';
 import { fetchPartOfSpeech } from '@/app/api/vocabulary/route';
 import {
-	Checkbox,
-	FormControl,
-	FormControlLabel,
-	InputLabel,
-	MenuItem,
-	Select,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
+  Select,
 } from '@mui/material';
 
 interface WordFormDialogProps {
-	open: boolean;
-	onClose: () => void;
-	onAddWord: (newWord: WordType) => void;
-	initialWord?: WordType | null;
+  open: boolean;
+  onClose: () => void;
+  onAddWord: (newWord: WordType) => void;
+  initialWord?: WordType | null;
 }
 
 const WordFormDialog: React.FC<WordFormDialogProps> = ({
-	open,
-	onClose,
-	onAddWord,
-	initialWord,
+  open,
+  onClose,
+  onAddWord,
+  initialWord,
 }) => {
-	const [word, setWord] = useState(initialWord?.word ?? '');
-	const [meaning, setMeaning] = useState(initialWord?.meaning ?? '');
-	const [partOfSpeech, setPartOfSpeech] = useState(initialWord?.partOfSpeechId ?? '');
-	const [pronunciation, setPronunciation] = useState(initialWord?.pronunciation ?? '');
-	const [exampleSentence, setExampleSentence] = useState(initialWord?.exampleSentence ?? '');
-	const [synonyms, setSynonyms] = useState(initialWord?.synonyms ?? '');
-	const [antonyms, setAntonyms] = useState(initialWord?.antonyms ?? '');
-	const [url, setUrl] = useState(initialWord?.url ?? '');
-	const [memorized, setMemorized] = useState(initialWord?.memorized ?? false);
-	const [partOfSpeechList, setPartOfSpeechList] = useState<PartOfSpeech[]>([]);
+  const [word, setWord] = useState(initialWord?.word ?? '');
+  const [meaning, setMeaning] = useState(initialWord?.meaning ?? '');
+  const [partOfSpeech, setPartOfSpeech] = useState(initialWord?.partOfSpeechId ?? '');
+  const [pronunciation, setPronunciation] = useState(initialWord?.pronunciation ?? '');
+  const [exampleSentence, setExampleSentence] = useState(initialWord?.exampleSentence ?? '');
+  const [synonyms, setSynonyms] = useState(initialWord?.synonyms ?? '');
+  const [antonyms, setAntonyms] = useState(initialWord?.antonyms ?? '');
+  const [url, setUrl] = useState(initialWord?.url ?? '');
+  const [memorized, setMemorized] = useState(initialWord?.memorized ?? false);
+  const [partOfSpeechList, setPartOfSpeechList] = useState<PartOfSpeech[]>([]);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const data: PartOfSpeech[] = await fetchPartOfSpeech();
-				setPartOfSpeechList(data);
-			} catch (error) {
-				console.error('Failed to fetch part of speech:', error);
-			}
-		};
-		fetchData();
-	}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data: PartOfSpeech[] = await fetchPartOfSpeech();
+        setPartOfSpeechList(data);
+      } catch (error) {
+        console.error('Failed to fetch part of speech:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
-	useEffect(() => {
-		if (initialWord) {
-			setWord(initialWord.word ?? '');
-			setMeaning(initialWord.meaning ?? '');
-			setPartOfSpeech(initialWord.partOfSpeechId ?? '');
-			setPronunciation(initialWord.pronunciation ?? '');
-			setExampleSentence(initialWord.exampleSentence ?? '');
-			setSynonyms(initialWord.synonyms ?? '');
-			setAntonyms(initialWord.antonyms ?? '');
-			setUrl(initialWord.url ?? '');
-			setMemorized(initialWord.memorized ?? false);
-		} else {
-			setWord('');
-			setMeaning('');
-			setPartOfSpeech('');
-			setPronunciation('');
-			setExampleSentence('');
-			setSynonyms('');
-			setAntonyms('');
-			setUrl('');
-			setMemorized(false);
-		}
-	}, [initialWord]);
+  useEffect(() => {
+    if (initialWord) {
+      setWord(initialWord.word ?? '');
+      setMeaning(initialWord.meaning ?? '');
+      setPartOfSpeech(initialWord.partOfSpeechId ?? '');
+      setPronunciation(initialWord.pronunciation ?? '');
+      setExampleSentence(initialWord.exampleSentence ?? '');
+      setSynonyms(initialWord.synonyms ?? '');
+      setAntonyms(initialWord.antonyms ?? '');
+      setUrl(initialWord.url ?? '');
+      setMemorized(initialWord.memorized ?? false);
+    } else {
+      setWord('');
+      setMeaning('');
+      setPartOfSpeech('');
+      setPronunciation('');
+      setExampleSentence('');
+      setSynonyms('');
+      setAntonyms('');
+      setUrl('');
+      setMemorized(false);
+    }
+  }, [initialWord]);
 
-	const handleSubmit = (event: React.FormEvent) => {
-		event.preventDefault();
-		const newWord: WordType = {
-			id: initialWord?.id,
-			word,
-			meaning,
-			partOfSpeechId: Number(partOfSpeech),
-			pronunciation,
-			exampleSentence,
-			synonyms,
-			antonyms,
-			url,
-			memorized: false,
-			createdAt: new Date().toISOString(),
-			updatedAt: new Date().toISOString(),
-		};
-		onAddWord(newWord);
-		onClose();
-	};
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const newWord: WordType = {
+      id: initialWord?.id,
+      word,
+      meaning,
+      partOfSpeechId: Number(partOfSpeech),
+      pronunciation,
+      exampleSentence,
+      synonyms,
+      antonyms,
+      url,
+      memorized,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    };
+    onAddWord(newWord);
+    onClose();
+  };
 
-	return (
-		<Dialog open={open} onClose={onClose}>
-			<DialogTitle textAlign="center">{initialWord ? '単語を編集' : '単語を追加'}</DialogTitle>
-			<DialogContent>
-				<form onSubmit={handleSubmit}>
-					<TextField
-						autoFocus
-						margin="dense"
-						label="単語"
-						type="text"
-						fullWidth
-						value={word}
-						onChange={(e) => setWord(e.target.value)}
-					/>
-					<TextField
-						margin="dense"
-						label="意味"
-						type="text"
-						fullWidth
-						value={meaning}
-						onChange={(e) => setMeaning(e.target.value)}
-					/>
-					<FormControl fullWidth margin="dense">
-						<InputLabel id="part-of-speech-label">品詞</InputLabel>
-						<Select
-							labelId="part-of-speech-label"
-							value={partOfSpeech}
-							onChange={(e) => setPartOfSpeech(e.target.value as string)}
-						>
-							{partOfSpeechList.map((pos) => (
-								<MenuItem key={pos.id} value={pos.id}>
-									{pos.name}
-								</MenuItem>
-							))}
-						</Select>
-					</FormControl>
-					<TextField
-						margin="dense"
-						label="発音"
-						type="text"
-						fullWidth
-						value={pronunciation}
-						onChange={(e) => setPronunciation(e.target.value)}
-					/>
-					<TextField
-						margin="dense"
-						label="例文"
-						type="text"
-						fullWidth
-						value={exampleSentence}
-						onChange={(e) => setExampleSentence(e.target.value)}
-					/>
-					<TextField
-						margin="dense"
-						label="同義語"
-						type="text"
-						fullWidth
-						value={synonyms}
-						onChange={(e) => setSynonyms(e.target.value)}
-					/>
-					<TextField
-						margin="dense"
-						label="反義語"
-						type="text"
-						fullWidth
-						value={antonyms}
-						onChange={(e) => setAntonyms(e.target.value)}
-					/>
-					<TextField
-						margin="dense"
-						label="URL"
-						type="text"
-						fullWidth
-						value={url}
-						onChange={(e) => setUrl(e.target.value)}
-					/>
-					<FormControlLabel
-						control={
-							<Checkbox
-								checked={memorized}
-								onChange={(e) => setMemorized(e.target.checked)}
-								color="primary"
-							/>
-						}
-						label="覚えた"
-					/>
-					<DialogActions>
-						<Button onClick={onClose} color="primary">
-							キャンセル
-						</Button>
-						<Button type="submit" color="primary">
-							{initialWord ? '更新' : '追加'}
-						</Button>
-					</DialogActions>
-				</form>
-			</DialogContent>
-		</Dialog>
-	);
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle textAlign="center">{initialWord ? '単語を編集' : '単語を追加'}</DialogTitle>
+      <DialogContent>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="単語"
+            type="text"
+            fullWidth
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label="意味"
+            type="text"
+            fullWidth
+            value={meaning}
+            onChange={(e) => setMeaning(e.target.value)}
+          />
+          <FormControl fullWidth margin="dense">
+            <InputLabel id="part-of-speech-label">品詞</InputLabel>
+            <Select
+              labelId="part-of-speech-label"
+              value={partOfSpeech}
+              onChange={(e) => setPartOfSpeech(e.target.value as string)}
+            >
+              {partOfSpeechList.map((pos) => (
+                <MenuItem key={pos.id} value={pos.id}>
+                  {pos.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <TextField
+            margin="dense"
+            label="発音"
+            type="text"
+            fullWidth
+            value={pronunciation}
+            onChange={(e) => setPronunciation(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label="例文"
+            type="text"
+            fullWidth
+            value={exampleSentence}
+            onChange={(e) => setExampleSentence(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label="同義語"
+            type="text"
+            fullWidth
+            value={synonyms}
+            onChange={(e) => setSynonyms(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label="反義語"
+            type="text"
+            fullWidth
+            value={antonyms}
+            onChange={(e) => setAntonyms(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label="URL"
+            type="text"
+            fullWidth
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={memorized}
+                onChange={(e) => setMemorized(e.target.checked)}
+                color="primary"
+              />
+            }
+            label="覚えた"
+          />
+          <DialogActions>
+            <Button onClick={onClose} color="primary">
+              キャンセル
+            </Button>
+            <Button type="submit" color="primary">
+              {initialWord ? '更新' : '追加'}
+            </Button>
+          </DialogActions>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default WordFormDialog;
--- frontend/components/WordModal/WordModal.tsx
+++ frontend/components/WordModal/WordModal.tsx
@@ -1,138 +1,130 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Dialog, DialogTitle, DialogContent, Typography, Button, Box } from '@mui/material';
 import { WordType } from '@/types';
-import Link from 'next/link';
 import CheckIcon from '@mui/icons-material/Check';
 import './WordModal.css';
 import partOfSpeechIdToName from '@/utils/partOfSpeech';
 
 interface WordModalProps {
-	open: boolean;
-	onClose: () => void;
-	word: WordType | null;
-	setSelectedWord: React.Dispatch<React.SetStateAction<WordType | null>>;
-	handleMemorizedClick: (word: WordType) => void;
-	handleEditClick: () => void;
-	showDetails: boolean;
-	setShowDetails: React.Dispatch<React.SetStateAction<boolean>>;
+  open: boolean;
+  onClose: () => void;
+  word: WordType | null;
+  setSelectedWord: React.Dispatch<React.SetStateAction<WordType | null>>;
+  handleMemorizedClick: (word: WordType) => void;
+  handleEditClick: () => void;
+  showDetails: boolean;
+  setShowDetails: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const WordModal: React.FC<WordModalProps> = ({
-	open,
-	onClose,
-	word,
-	setSelectedWord,
-	handleMemorizedClick,
-	handleEditClick,
-	showDetails,
-	setShowDetails,
+  open,
+  onClose,
+  word,
+  setSelectedWord,
+  handleMemorizedClick,
+  handleEditClick,
+  showDetails,
+  setShowDetails,
 }) => {
-	const handleClose = () => {
-		setShowDetails(false);
-		setSelectedWord(null);
-		onClose();
-	};
+  const handleClose = () => {
+    setShowDetails(false);
+    setSelectedWord(null);
+    onClose();
+  };
 
-	const handleUpdateMemorized = (word: WordType) => {
-		handleMemorizedClick(word);
-	};
-	if (!word) return null;
+  const handleUpdateMemorized = (word: WordType) => {
+    handleMemorizedClick(word);
+  };
 
-	return (
-		<Dialog
-			open={open}
-			onClose={onClose}
-			PaperProps={{
-				sx: {
-					minWidth: '400px',
-					minHeight: '200px',
-					display: 'flex',
-					flexDirection: 'column',
-					justifyContent: 'space-between',
-					backgroundColor: 'transparent',
-				}, // Paperコンポーネントにスタイルを適用
-			}}
-		>
-			<DialogContent>
-				<Box className={`card ${showDetails ? 'is-flipped' : ''}`}>
-					<Box className={`card-inner`}>
-						<Box
-							className={`card-element ${showDetails ? 'is-flipped' : ''}`}
-							sx={{ backgroundColor: 'white' }}
-						>
-							{!showDetails ? (
-								<Box className="card-front-element" sx={{ backgroundColor: 'white', minHeight: '300px' }}>
-									<DialogTitle sx={{ textAlign: 'center', wordBreak: 'break-word' }}>
-										<Box display="flex" justifyContent="center" alignItems="center">
-											<Box sx={{ width: '24px', display: 'flex', justifyContent: 'center' }}>
-												{word.memorized && <CheckIcon sx={{ color: 'green' }} />}
-											</Box>
-											<Box sx={{ flexGrow: 1, wordBreak: 'break-word', textAlign: 'center' }}>{word.word}</Box>
-										</Box>
-									</DialogTitle>
-									<Box display="flex" justifyContent="center">
-										<Button onClick={handleClose}>閉じる</Button>
-										<Button onClick={() => setShowDetails(true)}>訳をみる</Button>
-									</Box>
-								</Box>
-							) : (
-								<Box className={`card-back-element`} sx={{ padding: '20px' }}>
-									<DialogTitle sx={{ textAlign: 'center', wordBreak: 'break-word' }}>
-										<Box display="flex" justifyContent="center" alignItems="center">
-											<Box sx={{ width: '24px', display: 'flex', justifyContent: 'center' }}>
-												{word.memorized && <CheckIcon sx={{ color: 'green' }} />}
-											</Box>
-											<Box sx={{ flexGrow: 1, wordBreak: 'break-word', textAlign: 'center' }}>{word.word}</Box>
-										</Box>
-									</DialogTitle>
-									<Typography variant="body2" color="text.secondary" sx={{ wordBreak: 'break-word' }}>
-										意味: {word.meaning}
-									</Typography>
-									<Typography variant="body2" color="text.secondary">
-										品詞: {partOfSpeechIdToName(word.partOfSpeechId)}
-									</Typography>
-									<Typography variant="body2" color="text.secondary">
-										発音記号: {word.pronunciation}
-									</Typography>
-									<Typography variant="body2" color="text.secondary">
-										例文: {word.exampleSentence}
-									</Typography>
-									<Typography variant="body2" color="text.secondary">
-										類義語: {word.synonyms}
-									</Typography>
-									<Typography variant="body2" color="text.secondary">
-										対義語: {word.antonyms}
-									</Typography>
-									{word.url && (
-										<Typography variant="body2" color="text.secondary">
-											URL:{' '}
-											<Link href={word.url} passHref>
-												<a target="_blank" rel="noopener noreferrer">
-													{word.url}
-												</a>
-											</Link>
-										</Typography>
-									)}
-									<Box display="flex" justifyContent="center" mb={1}>
-										<Button color="primary" onClick={() => handleUpdateMemorized(word)}>
-											{word.memorized ? 'チェック外す' : '覚えた'}
-										</Button>
-										<Button color="primary" onClick={handleEditClick}>
-											編集する
-										</Button>
-									</Box>
-									<Box display="flex" justifyContent="center">
-										<Button onClick={handleClose}>閉じる</Button>
-										<Button onClick={() => setShowDetails(false)}>戻る</Button>
-									</Box>
-								</Box>
-							)}
-						</Box>
-					</Box>
-				</Box>
-			</DialogContent>
-		</Dialog>
-	);
+  if (!word) return null;
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      PaperProps={{
+        sx: {
+          minWidth: '500px', // モーダルの最小幅を設定
+          minHeight: '300px', // モーダルの最小高さを設定
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          backgroundColor: 'transparent',
+        },
+      }}
+    >
+      <DialogContent sx={{ position: 'relative', minHeight: '300px' }}>
+        <Box className={`flip-card ${showDetails ? 'flipped' : ''}`} sx={{ minHeight: '300px' }}>
+          <Box className="flip-card-inner" sx={{ minHeight: '300px' }}>
+            <Box className="flip-card-front">
+              <DialogTitle sx={{ textAlign: 'center', wordBreak: 'break-word' }}>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Box sx={{ width: '24px', display: 'flex', justifyContent: 'center' }}>
+                    {word.memorized && <CheckIcon sx={{ color: 'green' }} />}
+                  </Box>
+                  <Box sx={{ wordBreak: 'break-word', textAlign: 'center' }}>{word.word}</Box>
+                </Box>
+              </DialogTitle>
+              <Box display="flex" justifyContent="center">
+                <Button onClick={handleClose}>閉じる</Button>
+                <Button onClick={() => setShowDetails(true)}>訳をみる</Button>
+              </Box>
+            </Box>
+            <Box className="flip-card-back">
+              <DialogTitle sx={{ textAlign: 'center', wordBreak: 'break-word' }}>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <Box sx={{ width: '24px', display: 'flex', justifyContent: 'center' }}>
+                    {word.memorized && <CheckIcon sx={{ color: 'green' }} />}
+                  </Box>
+                  <Box sx={{ flexGrow: 1, wordBreak: 'break-word', textAlign: 'center' }}>
+                    {word.word}
+                  </Box>
+                </Box>
+              </DialogTitle>
+              <Typography variant="body2" color="text.secondary" sx={{ wordBreak: 'break-word' }}>
+                意味: {word.meaning}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                品詞: {partOfSpeechIdToName(word.partOfSpeechId)}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                発音記号: {word.pronunciation}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                例文: {word.exampleSentence}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                類義語: {word.synonyms}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                対義語: {word.antonyms}
+              </Typography>
+              {word.url && (
+                <Typography variant="body2" color="text.secondary">
+                  URL:{' '}
+                  <a target="_blank" rel="noopener noreferrer" href={word.url}>
+                    {word.url}
+                  </a>
+                </Typography>
+              )}
+              <Box display="flex" justifyContent="center" mb={1}>
+                <Button color="primary" onClick={() => handleUpdateMemorized(word)}>
+                  {word.memorized ? 'チェック外す' : '覚えた'}
+                </Button>
+                <Button color="primary" onClick={handleEditClick}>
+                  編集する
+                </Button>
+              </Box>
+              <Box display="flex" justifyContent="center">
+                <Button onClick={handleClose}>閉じる</Button>
+                <Button onClick={() => setShowDetails(false)}>戻る</Button>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default WordModal;
