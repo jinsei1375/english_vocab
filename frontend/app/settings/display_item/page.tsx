0c71d1f5a16b1f21d71625a51cd36cfd2cce7115
murakami <murakami@kokochie.jp>
2025-01-14 14:24:03 +0900

    設定ページ追加

---
+++ frontend/app/settings/display_item/page.tsx
@@ -0,0 +1,5 @@
+import PageTitle from '@/components/PageTitle';
+
+export default function DisplayItem() {
+	return <PageTitle title="表示項目" />;
+}
--- frontend/app/settings/page.tsx
+++ frontend/app/settings/page.tsx
@@ -1,9 +1,16 @@
 import PageTitle from '@/components/PageTitle';
+import { ListItemButton, ListItemText } from '@mui/material';
+import Link from 'next/link';
 
 export default async function Settings() {
 	return (
 		<>
 			<PageTitle title="設定" />
+			<Link href="/settings/display_item" passHref>
+				<ListItemButton>
+					<ListItemText primary="表示項目" />
+				</ListItemButton>
+			</Link>
 		</>
 	);
 }
