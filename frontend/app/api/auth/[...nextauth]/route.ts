90de7b85d0123e1d14104c3df8d7d4ca9b07cdfd
Shinsei <murakami.shinsei.zn@gmail.com>
2025-02-11 19:38:17 +0900

    wip

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -3,68 +3,63 @@ import GoogleProvider from 'next-auth/providers/google';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 declare module 'next-auth' {
-	interface Session {
-		user: {
-			id: string;
-			name: string;
-			email: string;
-		};
-	}
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+    };
+  }
 }
 
 export const authOptions: AuthOptions = {
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID!,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-		}),
-	],
-	callbacks: {
-		async signIn({ profile }) {
-			const email = profile?.email;
-			const name = profile?.name;
-			const google_id = profile?.sub;
-			if (!email || !name || !google_id) {
-				return false;
-			}
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ profile }) {
+      const email = profile?.email;
+      const name = profile?.name;
+      const google_id = profile?.sub;
+      if (!email || !name || !google_id) {
+        return false;
+      }
 
-			try {
-				// バックエンドのAPIエンドポイントにユーザー情報を送信
-				const response = await fetch(`${apiUrl}/api/users`, {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify({ google_id, name, email }),
-				});
+      try {
+        // バックエンドのAPIエンドポイントにユーザー情報を送信
+        const response = await fetch(`${apiUrl}/api/users`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ google_id, name, email }),
+        });
 
-				if (!response.ok) {
-					throw new Error(`HTTP error! status: ${response.status}`);
-				}
-			} catch {
-				return false;
-			}
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+      } catch {
+        return false;
+      }
 
-			return true;
-		},
-		async session({ session, token }) {
-			if (token?.id && session.user) {
-				session.user.id = token.id as string;
-			}
-			return session;
-		},
-		async jwt({ token, user }) {
-			if (user) {
-				token.id = user.id;
-			}
-			return token;
-		},
-	},
-	session: {
-		strategy: 'jwt',
-		maxAge: 60 * 60 * 24, // 1 day
-	},
-	secret: process.env.NEXTAUTH_SECRET,
+      return true;
+    },
+    async session({ session, token }) {
+      if (token?.id && session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
 };
 
 export const GET = NextAuth(authOptions);
