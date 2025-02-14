43165dda1d0a2909a763b24ae2835afc46d87ece
murakami <murakami@kokochie.jp>
2025-02-14 09:41:14 +0900

    ページ横幅調整

--- frontend/app/settings/page.tsx
+++ frontend/app/settings/page.tsx
@@ -1,16 +1,16 @@
 import PageTitle from '@/components/PageTitle';
-import { ListItemButton, ListItemText } from '@mui/material';
+import { Box, ListItemButton, ListItemText } from '@mui/material';
 import Link from 'next/link';
 
 export default async function Settings() {
 	return (
-		<>
+		<Box sx={{ padding: '20px', maxWidth: '600px', margin: 'auto' }}>
 			<PageTitle title="設定" />
 			<Link href="/settings/display_item" passHref>
 				<ListItemButton>
 					<ListItemText primary="表示項目" />
 				</ListItemButton>
 			</Link>
-		</>
+		</Box>
 	);
 }
